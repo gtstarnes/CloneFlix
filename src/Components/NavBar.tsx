ccacffaded4bc1996fcec84d9621deb58a44c110
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-18 01:54:51 -0400

    added styles to navBar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <nav>
-        <h1>CloneFlix</h1>
-        <ul>
-            <li>Sign In</li>
-            <li>Sign Up</li>
-        </ul>
+    <nav className="flex justify-between items-center p-4 z-[100] absolute w-full">
+        <h1 className="text-4xl text-red-600 font-bold cursor-pointer">CloneFlix</h1>
+        <div>
+            <button className="pr-4">Sign In</button>
+            <button className="bg-red-600 px-6 py-2 rounded curosr-pointer">Sign Out</button>
+        </div>
     </nav>
   )
 }
