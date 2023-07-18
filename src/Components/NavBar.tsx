c095f4da1a781057d12302329b79b77f18b0d392
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-18 01:45:37 -0400

    initial navBar commit

---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+        <h1>CloneFlix</h1>
+        <ul>
+            <li>Sign In</li>
+            <li>Sign Up</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
