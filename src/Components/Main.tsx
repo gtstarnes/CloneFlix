45982ec3d6907faa17966bde4dccf6c2fcbe2668
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-19 02:43:15 -0400

    fixed typing glitch; added some styles to hero

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -3,22 +3,26 @@ import { useState, useEffect } from 'react';
 import requests from '../requests'
 
 const Main = () => {
-  const [movies, setMovies] = useState({});
-  const movie: object = movies[Math.floor(Math.random() * movies.length)];
+  const [movies, setMovies] = useState<object>({});
+  const movie = movies[Math.floor(Math.random() * movies.length)];
   
 
   useEffect(() => {
-    fetch(requests.requestPopular)
-    .then(response => response.json())
-    .then(response => setMovies(response.results))
-    .catch(err => console.error(err));
+      fetch(requests.requestPopular)
+      .then(response => response.json())
+      .then(response => setMovies(response.results))
+      .catch(err => console.error(err));
+    
   },[])
 
   return (
     <div className="w-full h-[550px]">
       <div className="w-full h-full">
-      <img src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie.title} />
+        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+        <div></div>
+        <img className="w-full h-full object-cover" src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
       </div>
+    
     
     </div>
   )
