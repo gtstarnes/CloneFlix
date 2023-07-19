bd0f8be40c465452e23c253e0e370652e74743bc
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-19 01:01:31 -0400

    added api requests

---
+++ src/requests.ts
@@ -0,0 +1,10 @@
+
+const requests = {
+    requestTv: "https://api.themoviedb.org/3/tv/top_rated?api_key=8afd6839e8a778ced0dc0b6500cd3adb",
+    requestUpcoming: "https://api.themoviedb.org/3/movie/upcoming?api_key=8afd6839e8a778ced0dc0b6500cd3adb",
+    requestTopRated: "https://api.themoviedb.org/3/movie/top_rated?api_key=8afd6839e8a778ced0dc0b6500cd3adb",
+    requestPopular: "https://api.themoviedb.org/3/movie/popular?api_key=8afd6839e8a778ced0dc0b6500cd3adb"
+}
+
+
+export default requests
