67ae1611e1d61c153898695d6308f89eff9f5eb4
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-19 03:08:07 -0400

    added more contents to hero

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -5,7 +5,7 @@ import requests from '../requests'
 const Main = () => {
   const [movies, setMovies] = useState<object>({});
   const movie = movies[Math.floor(Math.random() * movies.length)];
-  
+  const year = movie?.release_date.slice(0,4);
 
   useEffect(() => {
       fetch(requests.requestPopular)
@@ -15,12 +15,26 @@ const Main = () => {
     
   },[])
 
+  const shortenSummary = (string:string, num:number) => {
+    if(string?.length > num){
+      return string.slice(0, num) + ' . . .'
+    }
+  }
+
   return (
     <div className="w-full h-[550px]">
       <div className="w-full h-full">
         <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
-        <div></div>
         <img className="w-full h-full object-cover" src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+        <div className="absolute w-full top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
+          <div className="my-4">
+            <button className="bg-gray-300 text-black border-gray-300 py-2 px-5 w-[80px]">Play</button>
+            <button className="border border-gray-200 py-2 px-5 ml-4">Watch Later</button>
+          </div>
+          <p className="text-gray-400 text-md mb-4">{year}</p>
+          <p className="w-full md:max-w-[70%] lg:max-w-[50%] text-gray-300">Summary: {shortenSummary(movie?.overview, 150)}</p>
+        </div>
       </div>
     
     
