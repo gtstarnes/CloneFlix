9afdfa71e778127c0fa0033996543b4a935439a0
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-19 02:06:04 -0400

    added backdrop to hero

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -4,6 +4,8 @@ import requests from '../requests'
 
 const Main = () => {
   const [movies, setMovies] = useState({});
+  const movie: object = movies[Math.floor(Math.random() * movies.length)];
+  
 
   useEffect(() => {
     fetch(requests.requestPopular)
@@ -12,10 +14,16 @@ const Main = () => {
     .catch(err => console.error(err));
   },[])
 
-  console.log(movies)
   return (
-    <div>Imports</div>
+    <div className="w-full h-[550px]">
+      <div className="w-full h-full">
+      <img src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie.title} />
+      </div>
+    
+    </div>
   )
 }
+
+
 
 export default Main
