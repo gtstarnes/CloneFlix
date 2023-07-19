640a78a180c04932d6632a878a82df6c228158c8
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-19 02:42:54 -0400

    minor changes

--- src/requests.ts
+++ src/requests.ts
@@ -1,5 +1,5 @@
 
-const requests = {
+const requests= {
     requestTv: "https://api.themoviedb.org/3/tv/top_rated?api_key=8afd6839e8a778ced0dc0b6500cd3adb",
     requestUpcoming: "https://api.themoviedb.org/3/movie/upcoming?api_key=8afd6839e8a778ced0dc0b6500cd3adb",
     requestTopRated: "https://api.themoviedb.org/3/movie/top_rated?api_key=8afd6839e8a778ced0dc0b6500cd3adb",
