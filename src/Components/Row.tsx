a2b2e261f845984f775836ba3ec14472e6886074
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-20 01:41:11 -0400

    cleaned up code with movie component

--- src/Components/Row.tsx
+++ src/Components/Row.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import Movie from './Movie';
 
 const Row = ({title, fetchURL}) => {
     const [movies, setMovies] = useState<Array<T>>([]);
@@ -21,16 +21,7 @@ const Row = ({title, fetchURL}) => {
         <div className="relative flex items-center">
             <div id={'slider'}>
                 {movies.map((movie) => {
-                    return <div className="w-[200px] sm:w-[280px] md:w-[360px] lg:w-[440px] inline-block cursor-pointer relative p-2">
-                                <img className="w-full h-auto block" src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt={movie?.title} />
-                                <div className="absolute top-0 left-0 w-full h-full hover:bg-black/60 opacity-0 hover:opacity-100">
-                                    <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">{movie?.title}</p>
-                                    <p>
-                                        {favorite ? <FaHeart className="absolute top-4 right-4 text-gray-300" /> : <FaRegHeart className="absolute top-4 right-4 text-gray-300" />}
-                                    </p>
-                                </div>
-                            </div>
-                })}
+                    return <Movie movie={movie}  />})}
             </div>
         </div>
     </div>
