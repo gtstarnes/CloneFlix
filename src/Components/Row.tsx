e681f244b43cfc15352668df5ee4fafc454c8e1a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-20 02:11:28 -0400

    minor changes

--- src/Components/Row.tsx
+++ src/Components/Row.tsx
@@ -4,23 +4,19 @@ import Movie from './Movie';
 
 const Row = ({title, fetchURL}) => {
     const [movies, setMovies] = useState<Array<T>>([]);
-    const [favorite, setFavorite] = useState(false);
 
     useEffect(() => {
         fetch(fetchURL)
         .then(response => response.json())
         .then(response => setMovies(response.results))
         .catch(err => console.error(err));
-        console.log(movies)
     }, [fetchURL])
-
-
 
   return (
     <div>
         <h2 className="font-bold md:text-xl p-4">{title}</h2>
         <div className="relative flex items-center">
-            <div id={'slider'}>
+            <div id={'slider'} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth">
                 {movies.map((movie) => {
                     return <Movie key={movie.id} movie={movie}  />})}
             </div>
