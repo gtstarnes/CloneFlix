05e36ff16c5c38958496a40822baa8badbbf7c94
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-19 23:25:48 -0400

    added more rows and props

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Main from '../Components/Main';
 import Row from '../Components/Row';
+import requests from '../requests';
 
 const Home = () => {
   return (
     <>
       <Main />
-      <Row />
+      <Row title="Coming Soon" fetchURL={requests.requestUpcoming} />
+      <Row title="Popular on CloneFlix" fetchURL={requests.requestPopular} />
+      <Row title="Critically Acclaimed Movies" fetchURL={requests.requestTopRated} />
+      <Row title="Popular TV Shows" fetchURL={requests.requestTv} />
+      <Row title="Coming Soon" fetchURL={requests.requestUpcoming} />
     </>
 
   )
