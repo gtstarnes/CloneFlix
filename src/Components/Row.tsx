9d8a98e7913af741c50ce69bd5d42c4529b3f907
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-19 23:57:39 -0400

    set up fetch for movie rows

--- src/Components/Row.tsx
+++ src/Components/Row.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
-const Row = () => {
+const Row = ({title, fetchURL}) => {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        fetch(fetchURL)
+        .then(response => response.json())
+        .then(response => setMovies(response.results))
+        .catch(err => console.error(err));
+    }, [fetchURL])
+
   return (
-    <div>Row</div>
+    <div>
+        <h2 className="font-bold md:text-xl p-4">{title}</h2>
+        <div className="relative flex items-center">
+            <div id={'slider'}>
+
+            </div>
+        </div>
+    </div>
   )
 }
 
