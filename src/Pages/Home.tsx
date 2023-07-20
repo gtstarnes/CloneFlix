702191722795b015fd2cdf13f54e9d983e77a3fc
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-19 23:21:28 -0400

    added row to home page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Main from '../Components/Main';
+import Row from '../Components/Row';
 
 const Home = () => {
   return (
-    <Main />
+    <>
+      <Main />
+      <Row />
+    </>
+
   )
 }
 
