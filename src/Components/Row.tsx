c3c2e142ecbec73ea1279763031b7e16f56bc234
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-20 00:58:16 -0400

    added initial rows of movies

--- src/Components/Row.tsx
+++ src/Components/Row.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 
 const Row = ({title, fetchURL}) => {
-    const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState<Array<T>>([])
 
     useEffect(() => {
         fetch(fetchURL)
@@ -11,12 +11,18 @@ const Row = ({title, fetchURL}) => {
         .catch(err => console.error(err));
     }, [fetchURL])
 
+
+
   return (
     <div>
         <h2 className="font-bold md:text-xl p-4">{title}</h2>
         <div className="relative flex items-center">
             <div id={'slider'}>
-
+                {movies.map((movie) => {
+                    return <div className="w-[200px] sm:w-[260px] md:w-[320px] lg:w-[400px] inline-block cursor-pointer p-4">
+                        <img className="" src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt={movie?.title} />
+                    </div>
+                })}
             </div>
         </div>
     </div>
