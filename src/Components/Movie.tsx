774d4c15322a32c8628eaf1c751fe09897e9caa7
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-08-17 23:45:17 -0400

    minor style changes

--- src/Components/Movie.tsx
+++ src/Components/Movie.tsx
@@ -5,12 +5,12 @@ import { FaHeart, FaRegHeart } from 'react-icons/fa';
 const Movie = ({movie}) => {
     const [favorite, setFavorite] = useState(false);
   return (
-    <div className="w-[280px] sm:w-[280px] md:w-[360px] lg:w-[440px] inline-block cursor-pointer relative p-2">
+    <div className="w-[280px] sm:w-[280px] md:w-[360px] lg:w-[440px] inline-block cursor-pointer relative border mx-2">
         <img className="w-full h-auto block" src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt={movie?.title} />
-        <div className="absolute top-0 left-0 w-full h-full hover:bg-black/60 opacity-0 hover:opacity-100">
-            <p className="white-space-normal text-sm md:text-md lg:text-lg font-bold flex justify-center items-center h-full text-center">{movie?.title}</p>
+        <div className="absolute top-0 left-0 w-full h-full hover:bg-black/60 opacity-0 hover:opacity-100 border break-all">
+            <p className="white-space-normal text-sm md:text-md lg:text-lg font-bold flex flex-wrap justify-center items-center h-full text-center">{movie?.title}</p>
             <p>
-                {favorite ? <FaHeart className="absolute top-4 right-4 text-gray-300" /> : <FaRegHeart className="absolute top-4 right-4 text-gray-300" />}
+                {favorite ? <FaHeart className="absolute top-4 right-4 sm:top-3 sm:right-3 scale-125 md:scale-150 text-gray-300" /> : <FaRegHeart className="absolute top-2 right-2 sm:top-3 sm:right-3 text-gray-300 scale-125 md:scale-150" />}
             </p>
         </div>
     </div>
