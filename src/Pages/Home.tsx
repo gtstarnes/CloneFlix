cca2eab24dcce3c41ca40d6435454f9215d42d1b
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-08-18 00:20:38 -0400

    added row ids

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -7,14 +7,14 @@ const Home = () => {
   return (
     <>
       <Main />
-      <Row title="Coming Soon" fetchURL={requests.requestUpcoming} />
-      <Row title="Popular on CloneFlix" fetchURL={requests.requestPopular} />
-      <Row title="Critically Acclaimed Movies" fetchURL={requests.requestTopRated} />
-      <Row title="Frightfully Scary" fetchURL={requests.requestHorror} />
-      <Row title="Animated Movies" fetchURL={requests.requestAnimation} />
-      <Row title="Hilarious and Light-Hearted" fetchURL={requests.requestComedy} />
-      <Row title="Crime and Punishment" fetchURL={requests.requestCrime} />
-      <Row title="Dramas" fetchURL={requests.requestDrama} />
+      <Row rowID="1" title="Coming Soon" fetchURL={requests.requestUpcoming} />
+      <Row rowID="2" title="Popular on CloneFlix" fetchURL={requests.requestPopular} />
+      <Row rowID="3" title="Critically Acclaimed Movies" fetchURL={requests.requestTopRated} />
+      <Row rowID="4" title="Frightfully Scary" fetchURL={requests.requestHorror} />
+      <Row rowID="5" title="Animated Movies" fetchURL={requests.requestAnimation} />
+      <Row rowID="6" title="Hilarious and Light-Hearted" fetchURL={requests.requestComedy} />
+      <Row rowID="7" title="Crime and Punishment" fetchURL={requests.requestCrime} />
+      <Row rowID="8" title="Dramas" fetchURL={requests.requestDrama} />
     </>
 
   )
