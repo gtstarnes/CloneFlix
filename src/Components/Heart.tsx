32f8b973d2efd01df3b432045d08033598b28cae
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-01 18:41:34 -0400

    created heart component

---
+++ src/Components/Heart.tsx
@@ -0,0 +1,17 @@
+import {useState} from 'react'
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
+
+const Heart = () => {
+    const [favorite, setFavorite] = useState(false);
+    const changeFavorite = () => {
+      setFavorite(!favorite)
+    }
+  return (
+    <div>
+    {favorite ? <FaHeart className="absolute top-4 right-4 sm:top-3 sm:right-3 scale-125 md:scale-150 text-gray-300" onClick={changeFavorite} /> : 
+    <FaRegHeart className="absolute top-2 right-2 sm:top-3 sm:right-3 text-gray-300 scale-125 md:scale-150" onClick={changeFavorite} />}
+    </div>
+  )
+}
+
+export default Heart
