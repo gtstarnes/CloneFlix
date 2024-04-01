8df80d13f6893db500dc292d3f598ad603bb3bec
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-01 17:52:22 -0400

    created show component

---
+++ src/Components/Show.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState } from 'react';
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
+
+type Show = {
+    backdrop_path: string,
+    name: string
+}
+
+const Show = ({show} : {show: Show}) => {
+    const [favorite, setFavorite] = useState(false);
+  return (
+    <div className="w-[280px] sm:w-[280px] md:w-[360px] lg:w-[440px] inline-block cursor-pointer relative mx-2">
+        <img className="w-full h-auto block" src={`https://image.tmdb.org/t/p/w500/${show.backdrop_path}`} alt={show.name} />
+        <div className="absolute top-0 left-0 w-full h-full hover:bg-black/60 opacity-0 hover:opacity-100 break-all">
+            <p className="white-space-normal text-xs sm:text-sm md:text-md lg:text-lg font-bold flex flex-wrap justify-center items-center h-full text-center">{show.name}</p>
+            <p>
+                {favorite ? <FaHeart className="absolute top-4 right-4 sm:top-3 sm:right-3 scale-125 md:scale-150 text-gray-300" /> : <FaRegHeart className="absolute top-2 right-2 sm:top-3 sm:right-3 text-gray-300 scale-125 md:scale-150" />}
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Show
