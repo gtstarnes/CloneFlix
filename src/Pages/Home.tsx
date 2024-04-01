4c72f60de43782152fa536e64a2e0470f7900a3e
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-04-01 18:23:22 -0400

    added popular tv row

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -11,12 +11,14 @@ const Home = () => {
       <Row rowID="1" title="Coming Soon" fetchURL={requests.requestUpcoming} />
       <Row rowID="2" title="Popular on CloneFlix" fetchURL={requests.requestPopular} />
       <Row rowID="3" title="Critically Acclaimed Movies" fetchURL={requests.requestTopRated} />
+      <Row rowID="10" title="Popular shows on CloneFlix" fetchURL={requests.requestPopularTV} />
+      <Row rowID="9" title="Top Rated Shows" fetchURL={requests.requestTv} />
       <Row rowID="4" title="Frightfully Scary" fetchURL={requests.requestHorror} />
       <Row rowID="5" title="Animated Movies" fetchURL={requests.requestAnimation} />
       <Row rowID="6" title="Hilarious and Light-Hearted" fetchURL={requests.requestComedy} />
       <Row rowID="7" title="Crime and Punishment" fetchURL={requests.requestCrime} />
       <Row rowID="8" title="Dramas" fetchURL={requests.requestDrama} />
-      <Row rowID="9" title="Top Rated Shows" fetchURL={requests.requestTv} />
+      
     </>
 
   )
